b69991379625a6b6268f324ae44b9cce7644b454
Osama Elgebaly <osamaelgebaly78@gmail.com>
2025-05-19 14:10:12 +0300

    Preparing for deployment

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = []
 
@@ -154,6 +154,7 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = 'media/'
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
